0e02e99369912bcc2fa3fe0259aa1ab60bc1c010
ThisIsIPat <thisisipat@users.noreply.github.com>
2019-08-04 04:51:25 +0200

    Created project files

---
+++ src/main/java/me/phein/kiloplugins/mc/kilodungeons/KiloDungeonsPlugin.java
@@ -0,0 +1,15 @@
+package me.phein.kiloplugins.mc.kilodungeons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class KiloDungeonsPlugin extends JavaPlugin {
+    @Override
+    public void onLoad() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+}
