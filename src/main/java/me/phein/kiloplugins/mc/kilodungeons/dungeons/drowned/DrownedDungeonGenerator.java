3bd0a9305c50cb77813b4ad3b359761091e24531
ThisIsIPat <thisisipat@users.noreply.github.com>
2019-08-06 17:00:15 +0200

    Optimized readability for Generator

--- src/main/java/me/phein/kiloplugins/mc/kilodungeons/dungeons/drowned/DrownedDungeonGenerator.java
+++ src/main/java/me/phein/kiloplugins/mc/kilodungeons/dungeons/drowned/DrownedDungeonGenerator.java
@@ -10,9 +10,10 @@ import org.bukkit.util.noise.SimplexNoiseGenerator;
 public class DrownedDungeonGenerator {
     private final World world;
     private NoiseGenerator noiseGen;
-    private Randomizer<Material> blocks = new RandomizerBuilder<Material>()
+    private Randomizer<Material> floorPalette = new RandomizerBuilder<Material>()
             .addOption(Material.PRISMARINE, 2)
             .addOption(Material.PRISMARINE_BRICKS, 1)
+            .addOption(Material.DARK_PRISMARINE_SLAB, 0.5)
             .build();
     private Randomizer<Material> blocksAndStairs = new RandomizerBuilder<Material>()
             .addOption(Material.PRISMARINE, 3)
@@ -29,49 +30,53 @@ public class DrownedDungeonGenerator {
     public void generate(int originX, int originY, int originZ) {
         originY += 5; // TODO TEMPORARY
 
-        int radius = 5;
+        generateFloor(originX, originY, originZ);
+    }
 
+    private void generateFloor(int originX, int originY, int originZ) {
         // FLOOR - center
         world.getBlockAt(originX, originY, originZ).setType(Material.SEA_LANTERN);
 
         // FLOOR - x + z axis (Fat plus-shape)
-        for (int relHorIt = radius; true; relHorIt--) {
+        for (int relHorIt = 5; true; relHorIt--) {
             // Middle plus
-            generateFloor(originX + relHorIt, originY, originZ);
-            generateFloor(originX - relHorIt, originY, originZ);
-            generateFloor(originX, originY, originZ + relHorIt);
-            generateFloor(originX, originY, originZ - relHorIt);
+            generateBlockAt(floorPalette, originX + relHorIt, originY, originZ);
+            generateBlockAt(floorPalette, originX - relHorIt, originY, originZ);
+            generateBlockAt(floorPalette, originX, originY, originZ + relHorIt);
+            generateBlockAt(floorPalette, originX, originY, originZ - relHorIt);
 
             // Side (Clock-wise)
-            generateFloor(originX + relHorIt, originY, originZ + 1);
-            generateFloor(originX - relHorIt, originY, originZ - 1);
-            generateFloor(originX - 1, originY, originZ + relHorIt);
-            generateFloor(originX + 1, originY, originZ - relHorIt);
+            generateBlockAt(floorPalette, originX + relHorIt, originY, originZ + 1);
+            generateBlockAt(floorPalette, originX - relHorIt, originY, originZ - 1);
+            generateBlockAt(floorPalette, originX - 1, originY, originZ + relHorIt);
+            generateBlockAt(floorPalette, originX + 1, originY, originZ - relHorIt);
 
             // Side (Counter-clock-wise)
             // This is not needed in the most center iteration, as it was already generated clock-wise.
             if (relHorIt <= 1) break;
-            generateFloor(originX + relHorIt, originY, originZ - 1);
-            generateFloor(originX - relHorIt, originY, originZ + 1);
-            generateFloor(originX + 1, originY, originZ + relHorIt);
-            generateFloor(originX - 1, originY, originZ - relHorIt);
+            generateBlockAt(floorPalette, originX + relHorIt, originY, originZ - 1);
+            generateBlockAt(floorPalette, originX - relHorIt, originY, originZ + 1);
+            generateBlockAt(floorPalette, originX + 1, originY, originZ + relHorIt);
+            generateBlockAt(floorPalette, originX - 1, originY, originZ - relHorIt);
         }
 
         // FLOOR - round corners
-        for (int relXIt = 2; relXIt < radius; relXIt++) {
-            for (int relZIt = 2; relZIt < radius; relZIt++) {
-                if (relXIt == radius - 1 && relZIt == radius - 1) break;
-                generateFloor(originX + relXIt, originY, originZ + relZIt);
-                generateFloor(originX - relXIt, originY, originZ + relZIt);
-                generateFloor(originX + relXIt, originY, originZ - relZIt);
-                generateFloor(originX - relXIt, originY, originZ - relZIt);
+        for (int relXIt = 2; true; relXIt++) {
+            for (int relZIt = 2; relZIt < 5; relZIt++) {
+                if (relXIt == 4 && relZIt == 4) return;
+                generateBlockAt(floorPalette, originX + relXIt, originY, originZ + relZIt);
+                generateBlockAt(floorPalette, originX - relXIt, originY, originZ + relZIt);
+                generateBlockAt(floorPalette, originX + relXIt, originY, originZ - relZIt);
+                generateBlockAt(floorPalette, originX - relXIt, originY, originZ - relZIt);
             }
         }
     }
 
-    private void generateFloor(int x, int y, int z) {
-        double selector = (noiseGen.noise(0.8 * x, 0.8 * z) + 1.0) / 2;
-        Material randomBlock = blocks.getRandomValue(selector);
+    private static final double SELECTOR_SIZE_FACTOR = 0.7;
+
+    private void generateBlockAt(Randomizer<Material> palette, int x, int y, int z) {
+        double selector = (noiseGen.noise(SELECTOR_SIZE_FACTOR * x, SELECTOR_SIZE_FACTOR * y, SELECTOR_SIZE_FACTOR * z) + 1.0) / 2;
+        Material randomBlock = palette.getRandomValue(selector);
         world.getBlockAt(x, y, z).setType(randomBlock);
     }
 }
