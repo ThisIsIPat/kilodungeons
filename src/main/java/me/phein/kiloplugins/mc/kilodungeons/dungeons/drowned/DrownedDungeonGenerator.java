09614b7fe4094fe27902bd6ee0a228e0fa368603
ThisIsIPat <thisisipat@users.noreply.github.com>
2019-08-09 09:44:46 +0200

    Completed wall generation

--- src/main/java/me/phein/kiloplugins/mc/kilodungeons/dungeons/drowned/DrownedDungeonGenerator.java
+++ src/main/java/me/phein/kiloplugins/mc/kilodungeons/dungeons/drowned/DrownedDungeonGenerator.java
@@ -6,20 +6,20 @@ import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.Bisected;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.Directional;
 import org.bukkit.block.data.Waterlogged;
+import org.bukkit.block.data.type.Slab;
 import org.bukkit.util.noise.NoiseGenerator;
 import org.bukkit.util.noise.SimplexNoiseGenerator;
 
 public class DrownedDungeonGenerator {
     private final World world;
     private NoiseGenerator noiseGen;
-    private Randomizer<Material> brokenWallPalette = new RandomizerBuilder<Material>()
-            .addOption(Material.PRISMARINE_BRICKS, 0.5)
-            .addOption(Material.PRISMARINE_BRICK_STAIRS, 2)
-            .addOption(Material.PRISMARINE, 0.5)
-            .addOption(Material.PRISMARINE_STAIRS, 2.5)
+    private Randomizer<Material> stairPalette = new RandomizerBuilder<Material>()
+            .addOption(Material.PRISMARINE_BRICK_STAIRS, 2.5)
+            .addOption(Material.PRISMARINE_STAIRS, 2)
             .build();
     private Randomizer<Material> solidWallPalette = new RandomizerBuilder<Material>()
             .addOption(Material.PRISMARINE_BRICKS, 2)
@@ -29,12 +29,6 @@ public class DrownedDungeonGenerator {
             .addOption(Material.PRISMARINE_BRICKS, 1.7)
             .addOption(Material.PRISMARINE, 1.5)
             .addOption(Material.DARK_PRISMARINE_SLAB, 0.3)
-            .build();
-    private Randomizer<Material> blocksAndStairs = new RandomizerBuilder<Material>()
-            .addOption(Material.PRISMARINE, 3)
-            .addOption(Material.PRISMARINE_SLAB, 1)
-            .addOption(Material.PRISMARINE_BRICKS, 2)
-            .addOption(Material.PRISMARINE_BRICK_SLAB, 1)
             .build();
 
     public DrownedDungeonGenerator(World world) {
@@ -46,26 +40,93 @@ public class DrownedDungeonGenerator {
         originY += 5; // TODO TEMPORARY
 
         generateFloor(originX, originY, originZ);
-        for (int radiusIt = 0; radiusIt <= 1; radiusIt++) {
-            int radius = 4 + radiusIt;
-            int wallHeight = 5 - radiusIt;
+        generateWalls(originX, originY, originZ);
+        generateCeiling(originX, originY, originZ);
+    }
 
-            for (int sideOffset = 1; sideOffset >= -1; sideOffset--) {
-                generateWall(BlockFace.WEST, originX + radius, originY, originZ + sideOffset, wallHeight);
-                generateWall(BlockFace.EAST, originX - radius, originY, originZ + sideOffset, wallHeight);
-                generateWall(BlockFace.NORTH, originX + sideOffset, originY, originZ + radius, wallHeight);
-                generateWall(BlockFace.SOUTH, originX + sideOffset, originY, originZ - radius, wallHeight);
+    private void generateCeiling(int originX, int originY, int originZ) {
+        int ceilingHeight = originY + 6;
+
+        for (int relX = -1; relX <= 1; relX++) {
+            for (int relZ = -1; relZ <= 1; relZ++) {
+                if (relX == 0 && relZ == 0) world.getBlockAt(originX, ceilingHeight, originZ).setType(Material.SEA_LANTERN);
+                else generateBlockAt(floorPalette, originX + relX, ceilingHeight, originZ + relZ);
+            }
+        }
+
+        // RADIUS = 2
+        // Above
+        generateCeilingBase(originX, ceilingHeight, originZ, 2);
+
+        // RADIUS = 4
+        // Above
+        generateCeilingBase(originX, ceilingHeight - 2, originZ, 4);
+        generateBlockAt(stairPalette, originX + 4, ceilingHeight - 1, originZ, BlockFace.WEST, Bisected.Half.BOTTOM);
+        generateBlockAt(stairPalette, originX - 4, ceilingHeight - 1, originZ, BlockFace.EAST, Bisected.Half.BOTTOM);
+        generateBlockAt(stairPalette, originX, ceilingHeight - 1, originZ + 4, BlockFace.NORTH, Bisected.Half.BOTTOM);
+        generateBlockAt(stairPalette, originX, ceilingHeight - 1, originZ - 4, BlockFace.SOUTH, Bisected.Half.BOTTOM);
+
+        // RADIUS = 5
+        // Above
+        generateCeilingBase(originX, ceilingHeight - 4, originZ, 5);
+        generateBlockAt(stairPalette, originX + 5, ceilingHeight - 3, originZ, BlockFace.WEST, Bisected.Half.BOTTOM);
+        generateBlockAt(stairPalette, originX - 5, ceilingHeight - 3, originZ, BlockFace.EAST, Bisected.Half.BOTTOM);
+        generateBlockAt(stairPalette, originX, ceilingHeight - 3, originZ + 5, BlockFace.NORTH, Bisected.Half.BOTTOM);
+        generateBlockAt(stairPalette, originX, ceilingHeight - 3, originZ - 5, BlockFace.SOUTH, Bisected.Half.BOTTOM);
+
+        for (int sideOffset = -1; sideOffset <= 1; sideOffset++) {
+            // RADIUS = 2
+            // Below
+            generateBlockAt(stairPalette, originX + 2, ceilingHeight - 1, originZ + sideOffset, BlockFace.EAST, Bisected.Half.TOP);
+            generateBlockAt(stairPalette, originX - 2, ceilingHeight - 1, originZ + sideOffset, BlockFace.WEST, Bisected.Half.TOP);
+            generateBlockAt(stairPalette, originX + sideOffset, ceilingHeight - 1, originZ + 2, BlockFace.SOUTH, Bisected.Half.TOP);
+            generateBlockAt(stairPalette, originX + sideOffset, ceilingHeight - 1, originZ - 2, BlockFace.NORTH, Bisected.Half.TOP);
+
+            // RADIUS = 3
+            // Above
+            generateBlockAt(solidWallPalette, originX + 3, ceilingHeight - 1, originZ + sideOffset);
+            generateBlockAt(solidWallPalette, originX - 3, ceilingHeight - 1, originZ + sideOffset);
+            generateBlockAt(solidWallPalette, originX + sideOffset, ceilingHeight - 1, originZ + 3);
+            generateBlockAt(solidWallPalette, originX + sideOffset, ceilingHeight - 1, originZ - 3);
+            // Below
+            generateBlockAt(stairPalette, originX + 3, ceilingHeight - 2, originZ + sideOffset, BlockFace.EAST, Bisected.Half.TOP);
+            generateBlockAt(stairPalette, originX - 3, ceilingHeight - 2, originZ + sideOffset, BlockFace.WEST, Bisected.Half.TOP);
+            generateBlockAt(stairPalette, originX + sideOffset, ceilingHeight - 2, originZ + 3, BlockFace.SOUTH, Bisected.Half.TOP);
+            generateBlockAt(stairPalette, originX + sideOffset, ceilingHeight - 2, originZ - 3, BlockFace.NORTH, Bisected.Half.TOP);
+        }
+    }
+
+    // Small recurring pattern in the ceiling generation.
+    private void generateCeilingBase(int originX, int originY, int originZ, int radius) {
+        for (int sideOffset = -1; sideOffset <= 1; sideOffset++) {
+            if (sideOffset == 0) {
+                generateBlockAt(solidWallPalette, originX + radius, originY, originZ);
+                generateBlockAt(solidWallPalette, originX - radius, originY, originZ);
+                generateBlockAt(solidWallPalette, originX, originY, originZ + radius);
+                generateBlockAt(solidWallPalette, originX, originY, originZ - radius);
+            } else {
+                generateBlockAt(stairPalette, originX + radius, originY, originZ + sideOffset, BlockFace.WEST, Bisected.Half.BOTTOM);
+                generateBlockAt(stairPalette, originX - radius, originY, originZ + sideOffset, BlockFace.EAST, Bisected.Half.BOTTOM);
+                generateBlockAt(stairPalette, originX + sideOffset, originY, originZ + radius, BlockFace.NORTH, Bisected.Half.BOTTOM);
+                generateBlockAt(stairPalette, originX + sideOffset, originY, originZ - radius, BlockFace.SOUTH, Bisected.Half.BOTTOM);
             }
         }
     }
 
-    private void generateWall(BlockFace brokenFacing, int originX, int originY, int originZ, int maxHeight) {
-        int height = noiseGen.noise(originX, originZ) < 0.3 ? maxHeight : maxHeight - 1;    // Warning: Noise = {-1 ; 1}
+    private void generateWalls(int originX, int originY, int originZ) {
+        for (int radiusIt = 0; radiusIt <= 1; radiusIt++) {
+            int radius = 4 + radiusIt;
+            int wallHeight = 3 - 2 * radiusIt;
 
-        for (int relY = 1; relY <= height; relY++) {
-            generateBlockAt(solidWallPalette, originX, originY + relY, originZ);
+            for (int sideOffset = 1; sideOffset >= -1; sideOffset--) {
+                for (int relY = 1; relY <= wallHeight; relY++) {
+                    generateBlockAt(solidWallPalette, originX + radius, originY + relY, originZ + sideOffset);
+                    generateBlockAt(solidWallPalette, originX - radius, originY + relY, originZ + sideOffset);
+                    generateBlockAt(solidWallPalette, originX + sideOffset, originY + relY, originZ + radius);
+                    generateBlockAt(solidWallPalette, originX + sideOffset, originY + relY, originZ - radius);
+                }
+            }
         }
-        generateBlockAt(brokenWallPalette, originX, originY + height, originZ, brokenFacing);
     }
 
     private void generateFloor(int originX, int originY, int originZ) {
@@ -94,21 +155,47 @@ public class DrownedDungeonGenerator {
         }
     }
 
-    private static final double SELECTOR_SIZE_FACTOR = 0.7;
+    private static final double SELECTOR_SIZE_FACTOR = 0.7; // Defines how rapid the palette changes.
 
     private void generateBlockAt(Randomizer<Material> palette, int x, int y, int z) {
-        generateBlockAt(palette, x, y, z, null);
+        generateBlockAt(palette, x, y, z, null, null);
     }
 
-    private void generateBlockAt(Randomizer<Material> palette, int x, int y, int z, BlockFace face) {
+    private void generateBlockAt(Material material, int x, int y, int z) {
+        generateBlockAt(material, x, y, z, null, null);
+    }
+
+    private void generateBlockAt(Randomizer<Material> palette, int x, int y, int z, BlockFace face, Bisected.Half half) {
         double selector = (noiseGen.noise(SELECTOR_SIZE_FACTOR * x, SELECTOR_SIZE_FACTOR * y, SELECTOR_SIZE_FACTOR * z) + 1.0) / 2;
         Material randomBlock = palette.getRandomValue(selector);
 
+        generateBlockAt(randomBlock, x, y, z, face, half);
+    }
+
+    private void generateBlockAt(Material material, int x, int y, int z, BlockFace face, Bisected.Half half) {
+        System.out.println("Generating @x" + x + " y" + y + " z" + z);
         Block block = world.getBlockAt(x, y, z);
 
-        BlockData data = randomBlock.createBlockData();
-        if (data instanceof Directional) {
-            ((Directional) data).setFacing(face);
+        BlockData data = material.createBlockData();
+        if (face != null) {
+            if (data instanceof Directional) {
+                ((Directional) data).setFacing(face);
+            }
+        }
+        if (half != null) {
+            if (data instanceof Bisected) {
+                ((Bisected) data).setHalf(half);
+            }
+            if (data instanceof Slab) {
+                switch (half) {
+                    case TOP:
+                        ((Slab) data).setType(Slab.Type.TOP);
+                        break;
+                    case BOTTOM:
+                        ((Slab) data).setType(Slab.Type.BOTTOM);
+                        break;
+                }
+            }
         }
         if (data instanceof Waterlogged) {
             ((Waterlogged) data).setWaterlogged(true);
