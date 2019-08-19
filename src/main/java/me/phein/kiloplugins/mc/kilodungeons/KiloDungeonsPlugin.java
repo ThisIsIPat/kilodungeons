7a3a4c8a9262edf2e536d316ac9967ae8f97fe3a
ThisIsIPat <thisisipat@users.noreply.github.com>
2019-08-19 04:21:43 +0200

    Added hint in the ConfigUnknownVersionException

--- src/main/java/me/phein/kiloplugins/mc/kilodungeons/KiloDungeonsPlugin.java
+++ src/main/java/me/phein/kiloplugins/mc/kilodungeons/KiloDungeonsPlugin.java
@@ -5,14 +5,21 @@ import me.phein.kiloplugins.mc.kilodungeons.config.ConfigManager;
 import me.phein.kiloplugins.mc.kilodungeons.dungeons.drowned.DrownedDungeonPopulator;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.world.WorldInitEvent;
 import org.bukkit.generator.BlockPopulator;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class KiloDungeonsPlugin extends JavaPlugin implements Listener {
     private ConfigManager configManager;
+    private List<RuntimeException> exceptions = new LinkedList<>();
 
     @Override
     public void onLoad() {
@@ -29,7 +36,31 @@ public class KiloDungeonsPlugin extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onWorldInit(WorldInitEvent event) {
-        BlockPopulator drownedDungeonPopulator = new DrownedDungeonPopulator(event.getWorld(), this.getLogger(), configManager.getUpdatedConfig());
-        event.getWorld().getPopulators().add(drownedDungeonPopulator);
+        try {
+            BlockPopulator drownedDungeonPopulator = new DrownedDungeonPopulator(event.getWorld(), this.getLogger(), configManager.getUpdatedConfig());
+            event.getWorld().getPopulators().add(drownedDungeonPopulator);
+        } catch (RuntimeException e) {
+            exceptions.add(e);
+            throw e;
+        }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        if (!exceptions.isEmpty() && player.isOp()) {
+            for (RuntimeException exception : exceptions) {
+                Throwable exceptionIt = exception;
+                while (exceptionIt != null) {
+                    player.sendMessage(exceptionIt.getMessage());
+                    exceptionIt = exceptionIt.getCause();
+                }
+            }
+            player.sendMessage(ChatColor.RED + "KiloDungeons had some issues being activated properly." + ChatColor.YELLOW + " The errors that occured are shown above.");
+            player.sendMessage("");
+            player.sendMessage(ChatColor.YELLOW + "View the console for a detailed error report on startup. If you can't fix the errors, please type " + ChatColor.GOLD + ChatColor.BOLD + "/kdun" + ChatColor.YELLOW + " for support.");
+            player.sendMessage(ChatColor.GRAY + "This message is only shown to operators on the server.");
+        }
     }
 }
--- src/main/java/me/phein/kiloplugins/mc/kilodungeons/exception/ConfigUnknownVersionException.java
+++ src/main/java/me/phein/kiloplugins/mc/kilodungeons/exception/ConfigUnknownVersionException.java
@@ -5,6 +5,7 @@ import java.io.File;
 public class ConfigUnknownVersionException extends ConfigVersionException {
     public ConfigUnknownVersionException(File configFile, String version) {
         super("Config file \"" + configFile.getAbsolutePath() + "\" specifies to be in an unknown version: " + version + "\n" +
-                "Is the plugin up-to-date?");
+                "Is the plugin up-to-date?" + "\n" +
+                "The plugin has backed up the file and will function with the default settings upon the next restart.");
     }
 }
