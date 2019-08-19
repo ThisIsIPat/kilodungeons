dd1d087912c77aca03cd885c47ce4659e27be042
ThisIsIPat <thisisipat@users.noreply.github.com>
2019-08-19 03:36:45 +0200

    Added pre-release note to /kdun and added 1.13 compability

--- src/main/java/me/phein/kiloplugins/mc/kilodungeons/command/KiloDungeonsCommandExecutor.java
+++ src/main/java/me/phein/kiloplugins/mc/kilodungeons/command/KiloDungeonsCommandExecutor.java
@@ -13,12 +13,13 @@ public class KiloDungeonsCommandExecutor implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Entity) {
             sender.sendMessage("" + ChatColor.DARK_BLUE + ChatColor.BOLD + "Kilo" + ChatColor.DARK_PURPLE + "Dungeons"
-                    + ChatColor.GOLD + ChatColor.ITALIC + " v0.1"
+                    + ChatColor.GOLD + " v0.1"
                     + ChatColor.GRAY + " by IPatGamer and WolfHybrid23");
+            sender.sendMessage("" + ChatColor.DARK_GRAY + ChatColor.ITALIC + "Note: This is a pre-release version. More dungeons will be added in the future.");
             sender.sendMessage("");
-            sender.sendMessage(ChatColor.YELLOW + "Need help with this plugin? Join our "
+            sender.sendMessage(ChatColor.LIGHT_PURPLE + "Need help? Want to suggest features? Join our "
                     + ChatColor.BLUE + ChatColor.BOLD + "Discord"
-                    + ChatColor.YELLOW + " community by clicking on the happy face:");
+                    + ChatColor.LIGHT_PURPLE + " community by clicking on the happy face:");
             ComponentBuilder builder = new ComponentBuilder("\u25D5\u203F\u25D5");
             builder.color(net.md_5.bungee.api.ChatColor.GREEN);
             builder.event(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://discord.gg/YUN262D"));
@@ -26,8 +27,9 @@ public class KiloDungeonsCommandExecutor implements CommandExecutor {
             sender.spigot().sendMessage(builder.create());
         } else {
             sender.sendMessage("KiloDungeons v0.1 by IPatGamer and WolfHybrid23");
+            sender.sendMessage("Note: This is a pre-release version. More dungeons will be added in the future.");
             sender.sendMessage("");
-            sender.sendMessage("Need help with this plugin? Join our Discord community by using this link:");
+            sender.sendMessage("Need help? Want to suggest features? Join our Discord community by using this link:");
             sender.sendMessage("https://discord.gg/YUN262D");
         }
         return true;
